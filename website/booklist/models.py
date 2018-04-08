e9bdcf0f2496ba3ee48797986d4bf07549299316
snowyjone <w007878@hotmail.com>
2018-04-08 11:11:59 +0800

    add git ignore and Model of the books

--- website/booklist/models.py
+++ website/booklist/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    # This is the model of the books on the bookshelf.
+    
+    # Basic information of a book
+    title = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    isbn = models.CharField(max_length=20, primary_key=True)
+    price = models.DecimalField()
+
+    # Additional information of a book
+    introduction = models.CharField(max_length=2000, null=True, blank=True)
+    url = models.CharField(maxn_length=200, null=True, blank=True)
+    image = models.CharField(maxn_length=50, null=True, blank=True)
+
+    # Current status of the book
+    
--- website/bookshelf/settings.py
+++ website/bookshelf/settings.py
@@ -105,6 +105,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
+DEFAULT_CHARSET = 'utf-8'
+
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
@@ -112,7 +114,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
