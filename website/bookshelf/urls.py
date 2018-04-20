b44293d2d63b30a6a843e9060658581ed172d50a
snowyjone <w007878@hotmail.com>
2018-04-20 23:29:45 +0800

    new settings

--- website/bookshelf/urls.py
+++ website/bookshelf/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('booklist/', include('booklist.urls')),
     path('admin/', admin.site.urls),
-    path('booklist', include('booklist.urls')),
 ]
